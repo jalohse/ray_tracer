ad04102a588ca8616860e92a78e6ced279ea0b94
Jessica Lohse <jessicaalohse@gmail.com>
2015-06-26 18:40:39 -0400

    Created test image from second edition

---
+++ src/me/jessicaalohse/raytracer/images/Chapter3SecondEdImage.java
@@ -0,0 +1,30 @@
+package me.jessicaalohse.raytracer.images;
+
+import java.io.IOException;
+
+import me.jessicaalohse.raytracer.shapes.Sphere;
+import me.jessicaalohse.raytracer.shapes.Triangle;
+import me.jessicaalohse.raytracer.utilities.Image;
+import me.jessicaalohse.raytracer.utilities.RGB;
+
+public class Chapter3SecondEdImage {
+
+	int rowsColumns = 500;
+
+	public Chapter3SecondEdImage() {
+		Image image = new Image(rowsColumns, rowsColumns);
+		Sphere sphere = new Sphere(250, 250, -1000, 150, new RGB(0, 0, 255), 0);
+		Triangle triangle = new Triangle(new double[] { 300, 600, -800 },
+				new double[] { 0, 100, -1000 },
+				new double[] { 450, 20, -1000 }, new RGB(255, 0, 0), 0);
+		image.addSurface(sphere);
+		image.addSurface(triangle);
+		image.createImage();
+		try {
+			image.printImage("Chapter3SecondEdImage");
+			System.out.println("Created Chapter3SecondEdImage.png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/me/jessicaalohse/raytracer/images/Tests.java
+++ src/me/jessicaalohse/raytracer/images/Tests.java
@@ -4,6 +4,7 @@ public class Tests {
 
 	public static void main(String[] args) {
 		new Chapter2Image();
+		new Chapter3SecondEdImage();
 		new Chapter3Image();
 		new Chapter4Image();
 	}
